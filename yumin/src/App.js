9e1ef19ba8359b5d0540e9dcd15109c1cd15d0c0
TabrizPlv <tabrizpahlavi@gmail.com>
2023-10-24 18:35:59 +1100

    done

--- yumin/src/App.js
+++ yumin/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import heart from "./hand.png";
+import emoji from "./emoji.png";
 
 function App() {
+  const [yes, setYes] = useState(false);
+
+  const onNo = () => {
+    alert("WRONG ANSWER!!! TRY AGAIN");
+    setYes(false);
+  };
+
+  const onYes = () => {
+    setYes(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="content">
+        <p>Do you want to be my girlfriend?</p>
+        <img src={yes ? heart : emoji} alt="heart"></img>
+        <div className="buttons">
+          <button className="button" onClick={onYes}>
+            Yes
+          </button>
+          <button className="button" onClick={onNo}>
+            No
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
